87a6ccd147f04d8903660fb781d2191c951ad22a
Diogo Barbosa Silva Sousa <diogobarbosasilva@gmail.com>
2021-10-06 20:31:57 -0300

    Criando Threads

--- Curso/ByteBank/ByteBank.View/MainWindow.xaml.cs
+++ Curso/ByteBank/ByteBank.View/MainWindow.xaml.cs
@@ -36,17 +36,37 @@ namespace ByteBank.View
         {
             var contas = r_Repositorio.GetContaClientes();
 
+            // Divindo o trabalho em duas partes
+            var contas_parte1 = contas.Take(contas.Count() / 2);
+            var contas_parte2 = contas.Skip(contas.Count() / 2);
+
             var resultado = new List<string>();
 
             AtualizarView(new List<string>(), TimeSpan.Zero);
 
             var inicio = DateTime.Now;
 
-            foreach (var conta in contas)
+            // Dividindo o processamento em duas partes
+            Thread thread_parte1 = new Thread(() =>
             {
-                var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
-                resultado.Add(resultadoConta);
-            }
+                foreach(var conta in contas_parte1)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+
+            Thread thread_parte2 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte2)
+                {
+                    var resultadoProcessamento = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoProcessamento);
+                }
+            });
+
+            thread_parte1.Start();
+            thread_parte2.Start();
 
             var fim = DateTime.Now;
 
